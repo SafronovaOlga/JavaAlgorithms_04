81b86471adee28f048975126bb3a95e5f8b0e4d1
new_name <new_email>
2022-05-07 17:50:51 +0200

    cleaned my code from commits

--- src/test/java/IsPositiveNumberTest.java
+++ src/test/java/IsPositiveNumberTest.java
@@ -4,19 +4,6 @@ import org.junit.jupiter.api.Test;
 public class IsPositiveNumberTest {
 
     @Test
-
-    /**
-     *  public void testIsPositiveNumberPos () {
-     *
-     *         int number = 555;
-     *         boolean expectedResult = true;
-     *
-     *         IsPositiveNumber isPositiveNumber = new IsPositiveNumber();
-     *         boolean actualResult = isPositiveNumber.getIsPositiveNumberTrue(number);
-     *
-     *         Assertions.assertTrue(actualResult);
-     *     }
-     */
     public void testIsPositiveNumbersPos () {
         int number = 555;
         boolean expectedResult = true;
