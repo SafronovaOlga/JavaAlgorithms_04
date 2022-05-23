316edf543b2104b3333e0929b5094cf2514404e7
SafronovaOlga <olgasafronova.636531448@gmail.com>
2022-05-23 11:25:02 +0200

    Refactored SortArrayTest and SumOfTwoTest

--- src/test/java/SortArrayTest.java
+++ src/test/java/SortArrayTest.java
@@ -5,6 +5,7 @@ public class SortArrayTest {
 
     @Test
     public void testSortArray () {
+
         int[] array = {4, 3, 7, 12, 5, 2, 9, 4, 12};
         int[] expectedResult = {2, 3, 4, 4, 5, 7, 9, 12, 12};
 
--- src/test/java/SumOfTwoTest.java
+++ src/test/java/SumOfTwoTest.java
@@ -5,6 +5,7 @@ public class SumOfTwoTest {
 
     @Test
     public void testSumOfTwoHappyPath () {
+
         int[] array = {4, 3, 7, 12, 5, 2, 9, 4, 12};
         int sum = 12;
         int[][] expectedResult = {{3, 9}, {7, 5}};
